8b9af083ce3b0527638dbd38f687aec01f9701de
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-05-18 19:35:43 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    char op;
+    printf("Enter the operator:");
+    scanf("%c",&op);
+    printf("Enter the first number:");
+    scanf("%d",&a);
+    printf("Enter the second number:");
+    scanf("%d",&b);    
+    switch(op){
+        case '+':
+            printf("%d + %d = %d",a,b,a+b);
+            break;
+        case '-':
+            printf("%d - %d = %d",a,b,a-b);
+            break;
+        case '*':
+            printf("%d * %d = %d",a,b,a*b);
+            break;
+        case '/':
+            printf("%d / %d = %d",a,b,a/b);
+            break;
+        default:
+            printf("Invalid operator");            
+    }
+}
+
+// #include <stdio.h>
+
+// int main() {
+
+//   char op;
+//   double first, second;
+//   printf("Enter an operator (+, -, *, /): ");
+//   scanf("%c", &op);
+//   printf("Enter two operands: ");
+//   scanf("%lf %lf", &first, &second);
+
+//   switch (op) {
+//     case '+':
+//       printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+//       break;
+//     case '-':
+//       printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+//       break;
+//     case '*':
+//       printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+//       break;
+//     case '/':
+//       printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+//       break;
+//     // operator doesn't match any case constant
+//     default:
+//       printf("Error! operator is not correct");
+//   }
+
+//   return 0;
+// }
