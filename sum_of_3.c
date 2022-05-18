6c74cdcc0c2f41766f95286f2dbebd795d45c4bc
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-05-18 19:10:31 +0530

    Add files via upload

---
+++ sum_of_3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("Enter the first number : ");
+    scanf("%d",&a);
+    printf("Enter the second number : ");
+    scanf("%d",&b);
+    printf("Enter the third number : ");
+    scanf("%d",&c);
+    printf("%d + %d + %d = %d",a,b,c,a+b+c);
+    return 0;
+}
