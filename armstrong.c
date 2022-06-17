d328b782718ceabf0fc71c8201ebc740ee4a7d20
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-06-17 10:39:55 +0530

    Add files via upload

---
+++ armstrong.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int num,p,count=0,sum=0;
+    printf("Enter the number : ");
+    scanf("%d",&num);
+
+    p=num;
+    while(p!=0){
+        p/=10;
+        count++;
+    }
+
+    p=num;
+    while(p!=0){
+        sum+=pow(p%10,count);
+        p/=10;
+    }
+
+    if(sum==num){
+        printf("%d is an armstrong number",num);
+    } else{
+        printf("%d is not an armstrong number",num);
+    }
+
+    return 0;
+}
