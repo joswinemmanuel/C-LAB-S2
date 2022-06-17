a2587254b4c4a97e225061e94a3d3ee7437da4e7
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-06-17 10:21:22 +0530

    Add files via upload

---
+++ bin_to_deci.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int binary,bin,base=1,deci=0;
+    printf("Enter the binary to be converted to decimal : ");
+    scanf("%d",&binary);
+    bin=binary;
+    while(bin!=0){
+        deci+=base*(bin%10);
+        base*=2;
+        bin/=10;
+    }
+    printf("Binary %d can be represented in decimal as %d",binary,deci);
+}
