7e366864acd8ef7059eb255f89e719adeb8959f2
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-06-26 11:17:07 +0530

    Create Question5.c

---
+++ Work-1/Question5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int matrix[10][10],length,trace=0;
+    printf("Enter the number of columns and rows of the square matrix : ");
+    scanf("%d",&length);
+    for(int i=0;i<length;i++){
+        for(int j=0;j<length;j++){
+            printf("Enter the value of matrix[%d][%d] : ",i,j);
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(i==j){
+                trace+=matrix[i][j];
+            }
+        }
+    }
+    printf("The trace of the matrix is : %d ",trace);
+}
