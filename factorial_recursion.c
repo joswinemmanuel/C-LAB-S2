0029ca7baef7ed40a3da3c432ca2b1d05d1e3eb7
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-07-01 13:56:26 +0530

    Add files via upload

---
+++ factorial_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int fact(int n);
+
+int main(){
+	int number;
+	printf("Enter the number : ");
+	scanf("%d",&number);
+	printf("Factorial of number is : %d",fact(number));
+	return 0;
+}
+
+int fact(int n){
+	if(n<=1)
+		return 1;
+	else
+		return n*fact(n-1);
+}
+
+
