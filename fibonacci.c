f9ebde884932ad9bfe2cfbc1e665f181002078a7
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-07-06 15:05:33 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int a=0,b=1,c,limit;
+	printf("Enter the limit : ");
+	scanf("%d", &limit);
+	
+	for(int i=0;i<limit;i++){
+		c = a + b;
+		printf("%d\n", a);
+		a = b;
+		b = c;
+	}
+}
