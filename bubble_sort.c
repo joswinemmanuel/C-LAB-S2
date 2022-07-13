6e9fa38807216e1b5e9c48178095fed908814f94
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-07-13 16:04:55 +0530

    Add files via upload

---
+++ bubble_sort.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+void bubble_sort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                arr[j]=arr[j]+arr[j+1];
+                arr[j+1]=arr[j]-arr[j+1];
+                arr[j]=arr[j]-arr[j+1];
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+}
+
+int main(){
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        printf("Enter value of index %d: ",i);
+        scanf("%d",&arr[i]);
+    }
+    printf("Sorted array: ");
+    bubble_sort(arr,n);
+}
