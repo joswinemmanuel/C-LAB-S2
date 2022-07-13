47bc03faa164322fbbf421e67756410b170619ec
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-07-13 14:54:03 +0530

    Add files via upload

---
+++ pointer_example.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int data=20, *p, carrier;  // declaring a pointer
+	p = &data;                 // initializing a pointer by giving the passing the address of data
+	carrier = *p;              // dereferencing the value in the pointer a assigning it to carrier variable
+	printf("%d", carrier);
+	return 0;
+}
+	
