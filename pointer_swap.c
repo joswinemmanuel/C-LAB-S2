6cb45cd34b1b4ece99badeb12636f9a1e75b6263
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-07-13 15:03:52 +0530

    Add files via upload

---
+++ pointer_swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void swap_variable(int *num1, int *num2);
+
+int main(){
+	int a=10, b=99;
+	printf("BEFORE SWAPPING\n");
+	printf("A is %d & B is %d\n", a, b);
+	swap_variable(&a, &b);
+	printf("BEFORE SWAPPING\n");
+	printf("A is %d & B is %d\n", a, b);
+	return 0;
+}
+
+void swap_variable(int *num1, int *num2){
+	int temp;
+	temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
