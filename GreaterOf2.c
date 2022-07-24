8230b3502b5fd9c0ab64d9af6e89c271a3db2b64
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-07-25 00:55:24 +0530

    Update GreaterOf2.c

--- GreaterOf2.c
+++ GreaterOf2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-//greater of two inputted numbers 
+//to find greater of two inputted numbers 
 int main(){
 	int num1,num2;
 	printf("This is a program to find the greater of the two numbers\n");
