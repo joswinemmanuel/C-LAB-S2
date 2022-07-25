ffd69e0ed7413a643e743d4ce3f44fe234539df4
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-07-25 11:27:50 +0530

    Add files via upload

--- file_demo.c
+++ file_demo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
 	FILE *fp;
-	fp = fopen("demo_file.txt", "w");
+	fp = fopen("file_demo.txt", "w");
 	fprintf(fp, "%d %d %d", 1, 2, 3);
 	fprintf(fp, "%s", "\nJoswin's gonna make it");
 	return 0;
