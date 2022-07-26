521b2759fdf26a89e9c86b3896316fcf72cb9fba
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-07-26 21:09:27 +0530

    Add files via upload

---
+++ string_concat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int j;
+    char s1[50], s2[50];
+    printf("Enter the first string : ");
+    scanf("%s", s1);
+    printf("Enter the second string : ");
+    scanf("%s", s2);
+    
+    j = strlen(s2);
+    for(int i=0; s2[i] != '\0'; i++){
+        s1[j+i] = s2[i];
+    }
+    
+    printf("Concatenated string : %s", s1);
+    return 0;
+}
