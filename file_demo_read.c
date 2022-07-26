3b32bddb2b0071e27642e64500b5657e118a8eba
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-07-26 21:25:12 +0530

    Add files via upload

--- file_demo_read.c
+++ file_demo_read.c
@@ -4,12 +4,13 @@ int main(){
     FILE *fp;
     fp = fopen("file_demo.txt", "r");
     int a, b, c;
-    char result;
+    char result, //re[10];
     fscanf(fp, "%d %d %d", &a, &b, &c);
     printf("%d %d %d", a, b, c);
     while((result = fgetc(fp)) != EOF){
         printf("%c", result);
     }
+    // fgets(re, 5, fp);
+    // printf("%s", re);
     fclose(fp);
-    return 0;
 }
