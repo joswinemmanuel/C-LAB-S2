57dec95a58ab2f8538136d7b9ed03dcae44a84c9
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-08-04 18:59:39 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-//to check if a number is armstrong or not
+//to check if the number is armstrong or not
 int main(){
     int num,p,count=0,sum=0;
     printf("Enter the number : ");
