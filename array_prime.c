1713c41523bf7cadfe81f4959a07aa51de83a090
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-08-28 23:06:13 +0530

    Add files via upload

--- array_prime.c
+++ array_prime.c
@@ -11,28 +11,31 @@ int main(){
     }
 
     for(int i=0; i<n; i++){
-        	int flag=1;
-            for(int i=2; i<arr[i]; i++){
-                if(arr[i] % i==0){
-                    flag=0;
-                    break;
-                }
+        int flag=1;
+        for(int j=2; j<arr[i]; j++){
+            if(arr[i] % j==0){
+                flag=0;
+                break;
             }
-            if(flag==0){
-                printf("%d is not prime",arr[i]);
-            } else {
-                printf("%d is prime",arr[i]);
-            }
-            return 0;
+        }
+        if(flag==0 || arr[i]<=1){
+            printf("%d is not prime\n",arr[i]);
+        } else{
+            printf("%d is prime\n",arr[i]);
+        }
     }
+    return 0;
 }
 
 /*
-Enter the length of the array : 5
 Enter the value of element 0 : 1
 Enter the value of element 1 : 2
 Enter the value of element 2 : 3
 Enter the value of element 3 : 4
 Enter the value of element 4 : 5
-1 is prime
+1 is not prime
+2 is prime
+3 is prime
+4 is not prime
+5 is prime
 */
