a3326f8d882220bd6b81e9e593c2754e56fddb02
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-14 00:25:25 +0530

    Update pointer_example.c

--- pointer_example.c
+++ pointer_example.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//Pointers
+//Pointers in c
 int main(){
 	int data=20, *p, carrier;  // declaring a pointer
 	p = &data;                 // initializing a pointer by giving the passing the address of data
